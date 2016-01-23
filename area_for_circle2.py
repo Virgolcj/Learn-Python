3a2d05ec392237b5380aa4596893c0f86b3dd10d
Virgolcj <virgolcj@gmail.com>
2016-01-24 03:24:56 +0800

    修改显示格式

--- area_for_circle2.py
+++ area_for_circle2.py
@@ -3,4 +3,9 @@ radius = eval(raw_input("请输入半径: "))
 area = radius * radius * 3.14159
 
 #Display results
-print "# 半径为", radius,"的圆面积为", area
+print "结果："
+#正常情况
+print "半径为",radius,"的圆面积为", area
+
+#去处之间空格
+print "半径为"+str(radius)+"的圆面积为"+str(area)
